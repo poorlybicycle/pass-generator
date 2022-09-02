e1d2a870394f219a0d44bbd79e652df0cd3deb09
Mime Jr <petravidovic8@gmail.com>
2022-09-01 22:07:35 -0400

    comments added to JS

--- script.js
+++ script.js
@@ -13,13 +13,14 @@ function writePassword() {
 
 }
 
+
 // Generate password function
 function generatePassword() {
   var passwordLength = prompt("Please enter the number of characters you want your password to contain. Must be no less than 8 and no more than 128 characters")
   if (parseInt(passwordLength) >= 8 && parseInt(passwordLength) <=  128) {
 
 
-    // Pop-up prompts
+    // Add pop-up prompts to choose password criteria
     var lowerCase = confirm("Do you want your password to contain lowercase letters?")
 
     var upperCase = confirm("Do you want your password to contain uppercase letters?")
@@ -30,7 +31,8 @@ function generatePassword() {
 
     var password = "";
 
-    //Variable that defines acceptable characters
+    //Variables that defines acceptable characters and execute code depending on whether 
+    //the conditions are true or false.
 
     var allChars = 'ABCDEFGHIJKLMNOPQURSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+'
 
@@ -58,7 +60,7 @@ function generatePassword() {
       password += allChars.charAt(Math.floor(Math.random() * allChars.length));
 
     };
-
+//displays password in textbox
     return password;
   }
   else {
